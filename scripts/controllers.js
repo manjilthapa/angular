c9926b56166851dd2cac5569fe401cd8d5783d57
manjilthapa <manjilthapa@gmail.com>
2016-01-26 22:56:06 +0100

    finished until week3 lec 1

---
+++ scripts/controllers.js
@@ -0,0 +1,107 @@
+'use strict';
+
+angular.module('confusionApp')
+
+        .controller('MenuController', ['$scope', 'menuFactory', function($scope, menuFactory) {
+            
+            $scope.tab = 1;
+            $scope.filtText = '';
+            $scope.showDetails = false;
+
+            $scope.dishes = menuFactory.getDishes();
+                        
+            $scope.select = function(setTab) {
+                $scope.tab = setTab;
+                
+                if (setTab === 2) {
+                    $scope.filtText = "appetizer";
+                }
+                else if (setTab === 3) {
+                    $scope.filtText = "mains";
+                }
+                else if (setTab === 4) {
+                    $scope.filtText = "dessert";
+                }
+                else {
+                    $scope.filtText = "";
+                }
+            };
+
+            $scope.isSelected = function (checkTab) {
+                return ($scope.tab === checkTab);
+            };
+    
+            $scope.toggleDetails = function() {
+                $scope.showDetails = !$scope.showDetails;
+            };
+        }])
+
+        .controller('ContactController', ['$scope', function($scope) {
+
+            $scope.feedback = {mychannel:"", firstName:"", lastName:"", agree:false, email:"" };
+            
+            var channels = [{value:"tel", label:"Tel."}, {value:"Email",label:"Email"}];
+            
+            $scope.channels = channels;
+            $scope.invalidChannelSelection = false;
+                        
+        }])
+
+        .controller('FeedbackController', ['$scope', function($scope) {
+            
+            $scope.sendFeedback = function() {
+                
+                console.log($scope.feedback);
+                
+                if ($scope.feedback.agree && ($scope.feedback.mychannel == "")) {
+                    $scope.invalidChannelSelection = true;
+                    console.log('incorrect');
+                }
+                else {
+                    $scope.invalidChannelSelection = false;
+                    $scope.feedback = {mychannel:"", firstName:"", lastName:"", agree:false, email:"" };
+                    $scope.feedback.mychannel="";
+                    $scope.feedbackForm.$setPristine();
+                    console.log($scope.feedback);
+                }
+            };
+        }])
+
+        .controller('DishDetailController', ['$scope', 'menuFactory', function($scope, menuFactory) {
+
+           var dish = menuFactory.getDish(3);
+            $scope.dish = dish;
+        }])
+
+        .controller('DishCommentController', ['$scope', function($scope) {
+           
+             $scope.showInfo = false;
+            
+            $scope.toggleInfo = function() {
+                $scope.showInfo = !$scope.showInfo;
+            };
+            
+            //Step 1: Create a JavaScript object to hold the comment from the form
+             $scope.comments = {author:"", rating:"5", date:"", comment:"" };
+             
+            
+            $scope.submitComment = function () {
+               
+                $scope.showInfo = false;
+               
+                //Step 2: This is how you record the date
+               // "The date property of your JavaScript object holding the comment"
+                $scope.comments.date= new Date().toISOString();
+                // console.log($scope.comments);
+                // Step 3: Push your comment into the dish's comment array
+                $scope.dish.comments.push($scope.comments);
+                
+                //Step 4: reset your form to pristine
+                 $scope.commentForm.$setPristine();
+             
+                //Step 5: reset your JavaScript object that holds your comment
+                $scope.comments = {author:"", rating:"5", date:"", comment:"" };
+            }
+        }])
+
+;
