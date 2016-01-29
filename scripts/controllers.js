3337e6889bf9c2dfc7df0ab169d249746fc847a1
manjilthapa <manjilthapa@gmail.com>
2016-01-29 12:42:02 +0100

    week 4 lecture 2 finished

--- scripts/controllers.js
+++ scripts/controllers.js
@@ -7,8 +7,21 @@ angular.module('confusionApp')
             $scope.tab = 1;
             $scope.filtText = '';
             $scope.showDetails = false;
+            $scope.message = "Loading...";
+            $scope.showMenu = false;
 
-            $scope.dishes= menuFactory.getDishes();
+            $scope.dishes= {};
+            
+            menuFactory.getDishes()
+            .then(
+                function(response){
+                    $scope.dishes = response.data;
+                    $scope.showMenu = true;
+                },
+                function(response){
+                    $scope.message = "Error: " + response.status + " " + response.statusText;
+                }
+            );
 
                         
             $scope.select = function(setTab) {
@@ -69,10 +82,21 @@ angular.module('confusionApp')
         }])
 
         .controller('DishDetailController', ['$scope', '$stateParams', 'menuFactory', function($scope, $stateParams, menuFactory) {
-
-            var dish= menuFactory.getDish(parseInt($stateParams.id,10));
             
-            $scope.dish = dish;
+            $scope.message = "Loading ...";
+            $scope.showDish = false;
+            $scope.dish = {};
+            
+            menuFactory.getDish(parseInt($stateParams.id,10))
+            .then(
+                function(response){
+                    $scope.dish = response.data;
+                    $scope.showDish = true;
+                },
+                function(response){
+                    $scope.message = "Error: " + response.status + " " + response.statusText;
+                }
+            );
             
         }])
 
@@ -96,8 +120,20 @@ angular.module('confusionApp')
         // implement the IndexController and About Controller here
         .controller('IndexController', ['$scope', 'menuFactory', 'corporateFactory', function($scope, menuFactory, corporateFactory){
     
-            var dish = menuFactory.getDish(0);
-            $scope.dish = dish;
+         
+            $scope.dish = {};
+            $scope.message = "Loading ...";
+            $scope.showDish = false;
+            
+            menuFactory.getDish(0).then(
+                function(response){
+                    $scope.dish = response.data;
+                    $scope.showDish = true;
+                },
+                function(response){
+                    $scope.message = "Error: " + response.status + " " + response.statusText;
+                }
+            );
             
             var promotion = menuFactory.getPromotion(0);
             $scope.promotion = promotion;
